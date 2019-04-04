1d27a84b1d40faaca6f8d984d1a4161e6ea7ad2d
frozolotl <frozolotl@protonmail.com>
2019-04-04 19:50:00 +0200

    implement Display for MStr

--- src/mutf8.rs
+++ src/mutf8.rs
@@ -1,9 +1,13 @@
 //! Modified UTF-8 string handling
 
 use crate::error::*;
-use std::borrow::{Borrow, ToOwned};
-use std::char;
-use std::ops::Deref;
+use std::{
+    borrow::{Borrow, ToOwned},
+    char,
+    fmt::{self, Write},
+    ops::Deref,
+    str,
+};
 
 /// A Modified UTF-8 string slice, like [str].
 #[derive(Eq, PartialEq, Hash)]
@@ -46,42 +50,9 @@ impl MStr {
             type Item = char;
 
             fn next(&mut self) -> Option<char> {
-                let b1 = self.inner[self.cursor];
-                if b1 & 0b1000_0000 == 0b0000_0000 {
-                    // single byte case
-                    self.cursor += 1;
-                    return Some(b1 as char);
-                }
-
-                let b2 = self.inner[self.cursor + 1];
-                if b1 & 0b1110_0000 == 0b1100_0000 {
-                    // two byte case
-                    self.cursor += 2;
-                    let c1 = u32::from(b1 & 0b0001_1111) << 6;
-                    let c2 = u32::from(b2 & 0b0011_1111);
-                    return Some(unsafe { char::from_u32_unchecked(c1 | c2) });
-                }
-
-                let b3 = self.inner[self.cursor + 2];
-                if b1 == 0b1110_1101 {
-                    // six byte case
-                    let b5 = self.inner[self.cursor + 4];
-                    let b6 = self.inner[self.cursor + 5];
-                    self.cursor += 6;
-
-                    let c2 = u32::from(b2 & 0b0000_1111) << 16;
-                    let c3 = u32::from(b3 & 0b0011_1111) << 10;
-                    let c5 = u32::from(b5 & 0b0000_1111) << 6;
-                    let c6 = u32::from(b6 & 0b0011_1111);
-                    return Some(unsafe { char::from_u32_unchecked(0x10000 | c2 | c3 | c5 | c6) });
-                }
-
-                // three byte case
-                self.cursor += 3;
-                let c1 = u32::from(b1 & 0b0000_1111) << 12;
-                let c2 = u32::from(b2 & 0b0011_1111) << 6;
-                let c3 = u32::from(b3 & 0b0011_1111);
-                Some(unsafe { char::from_u32_unchecked(c1 | c2 | c3) })
+                let (size, ch) = unsafe { decode_mutf8_char(&self.inner[self.cursor..]) };
+                self.cursor += size;
+                Some(ch)
             }
         }
 
@@ -147,6 +118,31 @@ impl ToOwned for MStr {
         MString {
             buf: self.inner.to_owned(),
         }
+    }
+}
+
+impl fmt::Display for MStr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut start = 0;
+        let mut i = 0;
+        while i < self.len() {
+            if self.inner[0] < 0x80 {
+                i += 1;
+            } else {
+                if i != start {
+                    // safe since everything from start to i is non-zero ascii
+                    f.write_str(unsafe { str::from_utf8_unchecked(&self.inner[start..i]) })?;
+                }
+
+                let (size, ch) = unsafe { decode_mutf8_char(&self.inner[i..]) };
+                i += size;
+
+                start = i;
+                f.write_char(ch)?;
+            }
+        }
+
+        Ok(())
     }
 }
 
@@ -228,4 +224,35 @@ fn is_mutf8_valid(v: &[u8]) -> bool {
     }
 
     true
+}
+
+/// Decodes a character and returns its size.
+/// The input bytes **must** be valid modified utf-8
+unsafe fn decode_mutf8_char(v: &[u8]) -> (usize, char) {
+    if v[0] & 0b1000_0000 == 0b0000_0000 {
+        // single byte case
+        return (1, v[0] as char);
+    }
+
+    if v[0] & 0b1110_0000 == 0b1100_0000 {
+        // two byte case
+        let c1 = u32::from(v[0] & 0b0001_1111) << 6;
+        let c2 = u32::from(v[1] & 0b0011_1111);
+        return (2, char::from_u32_unchecked(c1 | c2));
+    }
+
+    if v[0] == 0b1110_1101 {
+        // six byte case
+        let c2 = u32::from(v[1] & 0b0000_1111) << 16;
+        let c3 = u32::from(v[2] & 0b0011_1111) << 10;
+        let c5 = u32::from(v[4] & 0b0000_1111) << 6;
+        let c6 = u32::from(v[5] & 0b0011_1111);
+        return (6, char::from_u32_unchecked(0x10000 | c2 | c3 | c5 | c6));
+    }
+
+    // three byte case
+    let c1 = u32::from(v[0] & 0b0000_1111) << 12;
+    let c2 = u32::from(v[1] & 0b0011_1111) << 6;
+    let c3 = u32::from(v[2] & 0b0011_1111);
+    (3, char::from_u32_unchecked(c1 | c2 | c3))
 }
