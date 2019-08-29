f694aec092cc0718154c4e98c3299fde511318ac
frozolotl <frozolotl@protonmail.com>
2019-08-29 12:09:00 +0200

    add InnerClasses attribute

--- src/reader/attributes/class.rs
+++ src/reader/attributes/class.rs
@@ -57,3 +57,100 @@ impl fmt::Debug for NestHost {
         f.debug_struct("NestHost").finish()
     }
 }
+
+#[derive(Clone)]
+pub struct InnerClasses<'a> {
+    iter: InnerClassIter<'a>,
+}
+
+impl<'a> InnerClasses<'a> {
+    pub fn iter(&self) -> InnerClassIter<'a> {
+        self.iter.clone()
+    }
+}
+
+impl<'a> DecodeInto<'a> for InnerClasses<'a> {
+    fn decode_into(mut decoder: Decoder<'a>) -> Result<Self, DecodeError> {
+        // skip the count
+        decoder.advance(2)?;
+
+        Ok(InnerClasses {
+            iter: InnerClassIter { decoder },
+        })
+    }
+}
+
+impl<'a> fmt::Debug for InnerClasses<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("InnerClasses").finish()
+    }
+}
+
+#[derive(Clone)]
+pub struct InnerClassIter<'a> {
+    decoder: Decoder<'a>,
+}
+
+impl<'a> Iterator for InnerClassIter<'a> {
+    type Item = InnerClass;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.decoder.read().ok()
+    }
+}
+
+impl<'a> FusedIterator for InnerClassIter<'a> {}
+
+impl<'a> fmt::Debug for InnerClassIter<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("InnerClassIter").finish()
+    }
+}
+
+#[derive(Clone)]
+pub struct InnerClass {
+    outer_class: Option<cpool::Index<cpool::Class>>,
+    inner_class: cpool::Index<cpool::Class>,
+    inner_name: Option<cpool::Index<cpool::Utf8<'static>>>,
+    inner_access_flags: AccessFlags,
+}
+
+impl InnerClass {
+    pub fn outer_class(&self) -> Option<cpool::Index<cpool::Class>> {
+        self.outer_class
+    }
+
+    pub fn inner_class(&self) -> cpool::Index<cpool::Class> {
+        self.inner_class
+    }
+
+    pub fn inner_name(&self) -> Option<cpool::Index<cpool::Utf8<'static>>> {
+        self.inner_name
+    }
+
+    pub fn inner_access_flags(&self) -> AccessFlags {
+        self.inner_access_flags
+    }
+}
+
+impl<'a> Decode<'a> for InnerClass {
+    fn decode(decoder: &mut Decoder) -> Result<Self, DecodeError> {
+        let inner_class = decoder.read()?;
+        let outer_class = decoder.read()?;
+        let inner_name = decoder.read()?;
+        let inner_access_flags = decoder.read()?;
+
+        Ok(InnerClass {
+            inner_class,
+            outer_class,
+            inner_name,
+            inner_access_flags,
+        })
+    }
+}
+
+impl fmt::Debug for InnerClass {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("InnerClass").finish()
+    }
+}
