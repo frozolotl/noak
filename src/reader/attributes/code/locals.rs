b174b6d0f94cebedcc96e6ebc9878957a05853cb
frozolotl <frozolotl@protonmail.com>
2019-09-04 12:32:08 +0200

    add LocalVariableTypeTable attribute

--- src/reader/attributes/code/locals.rs
+++ src/reader/attributes/code/locals.rs
@@ -61,3 +61,60 @@ impl fmt::Debug for LocalVariable {
         f.debug_struct("LocalVariable").finish()
     }
 }
+
+pub type LocalVariableTypeTable<'a> = DecodeCountedCopy<'a, LocalVariableType>;
+pub type LocalVariableTypeIter<'a> = DecodeCounted<'a, LocalVariableType>;
+
+#[derive(Clone)]
+pub struct LocalVariableType {
+    start: code::Index,
+    end: code::Index,
+    name: cpool::Index<cpool::Utf8<'static>>,
+    signature: cpool::Index<cpool::Utf8<'static>>,
+    index: u16,
+}
+
+impl LocalVariableType {
+    pub fn range(&self) -> Range<code::Index> {
+        Range {
+            start: self.start,
+            end: self.end,
+        }
+    }
+
+    pub fn name(&self) -> cpool::Index<cpool::Utf8<'static>> {
+        self.name
+    }
+
+    pub fn signature(&self) -> cpool::Index<cpool::Utf8<'static>> {
+        self.signature
+    }
+
+    pub fn index(&self) -> u16 {
+        self.index
+    }
+}
+
+impl<'a> Decode<'a> for LocalVariableType {
+    fn decode(decoder: &mut Decoder) -> Result<Self, DecodeError> {
+        let start: u16 = decoder.read()?;
+        let end: u16 = decoder.read()?;
+        let name = decoder.read()?;
+        let signature = decoder.read()?;
+        let index = decoder.read()?;
+
+        Ok(LocalVariableType {
+            start: code::Index::new(start.into()),
+            end: code::Index::new(u32::from(start) + u32::from(end)),
+            name,
+            signature,
+            index,
+        })
+    }
+}
+
+impl fmt::Debug for LocalVariableType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("LocalVariableType").finish()
+    }
+}
