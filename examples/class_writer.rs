9652787af751b48a2717bc07114a448ba458dd8d
frozolotl <frozolotl@protonmail.com>
2019-09-05 22:07:22 +0200

    let the user specify the output path in example

--- examples/class_writer.rs
+++ examples/class_writer.rs
@@ -3,12 +3,15 @@
 use noak::writer::{ClassWriter, cpool};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = std::env::args()
+        .nth(1)
+        .expect("usage: `class_writer Output.class`");
     let mut class_writer = ClassWriter::new();
     let index = class_writer.insert_constant(cpool::Integer { value: 0 })?;
     println!("Index: {:?}", index);
 
     let bytes = class_writer.finish()?;
-    std::fs::write("Output.class", &bytes)?;
+    std::fs::write(path, &bytes)?;
 
     Ok(())
 }
