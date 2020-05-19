c7cc431f3ea51854b39fec3528a2903db21a424c
frozolotl <frozolotl@protonmail.com>
2020-05-19 23:07:48 +0200

    make write example easier to read
    Signed-off-by: frozolotl <frozolotl@protonmail.com>

--- examples/class_writer.rs
+++ examples/class_writer.rs
@@ -1,6 +1,6 @@
 //! This example writes an example class.
 
-use noak::writer::ClassWriter;
+use noak::writer::{cpool, ClassWriter};
 use noak::AccessFlags;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -17,25 +17,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .write_access_flags(AccessFlags::PUBLIC | AccessFlags::SUPER)?
         .write_this_class("Test")?
         .write_super_class("java/lang/Object")?
-        .write_interfaces(|writer| {
-            writer.write_simple("java/io/Serializable")?;
-            Ok(())
-        })?
         .write_fields(|writer| {
             writer.write(|writer| {
                 writer
                     .write_access_flags(
                         AccessFlags::PRIVATE | AccessFlags::STATIC | AccessFlags::FINAL,
                     )?
-                    .write_name("ZERO")?
-                    .write_descriptor("Ljava/lang/String;")?
-                    .write_attributes(|writer| {
-                        writer.write(|writer| {
-                            writer.write_constant_value("Hello World")?;
-                            Ok(())
-                        })?;
-                        Ok(())
-                    })?;
+                    .write_name("SOME_FIELD")?
+                    .write_descriptor("Ljava/lang/String;")?;
                 Ok(())
             })?;
             Ok(())
@@ -43,90 +32,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .write_methods(|writer| {
             writer.write(|writer| {
                 writer
-                    .write_access_flags(AccessFlags::PUBLIC)?
-                    .write_name("<init>")?
-                    .write_descriptor("()V")?
-                    .write_attributes(|writer| {
-                        writer
-                            .write(|writer| {
-                                writer.write_deprecated()?;
-                                Ok(())
-                            })?
-                            .write(|writer| {
-                                writer.write_exceptions(|writer| {
-                                    writer
-                                        .write_simple("java/io/IOException")?
-                                        .write_simple("java/lang/RuntimeException")?;
-                                    Ok(())
-                                })?;
-                                Ok(())
-                            })?
-                            .write(|writer| {
-                                writer.write_code(|writer| {
-                                    writer
-                                        .write_max_stack(0)?
-                                        .write_max_locals(0)?
-                                        .write_instructions(|writer| {
-                                            writer.write_return()?;
-
-                                            Ok(())
-                                        })?;
-                                    Ok(())
-                                })?;
-                                Ok(())
-                            })?;
-                        Ok(())
-                    })?;
-                Ok(())
-            })?;
-            writer.write(|writer| {
-                writer
-                    .write_access_flags(AccessFlags::PUBLIC)?
-                    .write_name("addOne")?
-                    .write_descriptor("(I)I")?
+                    .write_access_flags(AccessFlags::PUBLIC | AccessFlags::STATIC)?
+                    .write_name("main")?
+                    .write_descriptor("([Ljava/lang/String;)V")?
                     .write_attributes(|writer| {
                         writer.write(|writer| {
                             writer.write_code(|writer| {
                                 writer
-                                    .write_max_stack(2)?
-                                    .write_max_locals(1)?
+                                    .write_max_stack(6)?
+                                    .write_max_locals(2)?
                                     .write_instructions(|writer| {
-                                        let (def, def_ref) = writer.new_label()?;
-                                        let (zero, zero_ref) = writer.new_label()?;
-                                        let (one, one_ref) = writer.new_label()?;
-                                        let (two, two_ref) = writer.new_label()?;
-
-                                        writer.write_iload1()?.write_lookupswitch(|writer| {
-                                            writer
-                                                .write_default(def_ref)?
-                                                .write_pair(0, zero_ref)?
-                                                .write_pair(2, one_ref)?
-                                                .write_pair(4, two_ref)?;
-                                            Ok(())
-                                        })?;
-
                                         writer
-                                            .write_label(zero)?
-                                            .write_iconst1()?
-                                            .write_ireturn()?;
-
-                                        writer
-                                            .write_label(one)?
-                                            .write_iconst2()?
-                                            .write_ireturn()?;
-
-                                        writer
-                                            .write_label(two)?
-                                            .write_iconst3()?
-                                            .write_ireturn()?;
-
-                                        writer
-                                            .write_label(def)?
-                                            .write_bipush(1)?
-                                            .write_iload1()?
-                                            .write_iadd()?
-                                            .write_ireturn()?;
-
+                                            .write_getstatic(cpool::FieldRef::by(
+                                                "java/lang/System",
+                                                ("out", "Ljava/io/PrintStream;"),
+                                            ))?
+                                            .write_ldc(cpool::String::by("Hello, World!"))?
+                                            .write_invokevirtual(cpool::MethodRef::by(
+                                                "java/io/PrintStream",
+                                                ("println", "(Ljava/lang/String;)V"),
+                                            ))?
+                                            .write_return()?;
                                         Ok(())
                                     })?;
                                 Ok(())
